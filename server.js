968b6946f2397eb4b24d21c326f8169f4bc20309
SaurabhDesh <saurabh@spicetech.in>
2019-04-06 02:39:46 +0530

    mongolab url added

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 // Some necessary modules
-const express = require('express');
-var config = require('./server/configure');
-var mongoose = require('mongoose');
+const express = require("express");
+var config = require("./server/configure");
+var mongoose = require("mongoose");
 
 // Database - loginapp
-mongoose.connect('mongodb://localhost/loginapp');
+const uri =
+  "mongodb+srv://root:root@cluster0-cuslp.mongodb.net/loginapp?retryWrites=true";
+mongoose.connect(uri);
 // Create connection
 var db = mongoose.connection;
 
 // Create app
 let app = express();
-app=config(app);
-app.set('port', process.env.PORT || 3000);
-app.set('views', `${__dirname}/views`);
+app = config(app);
+app.set("port", process.env.PORT || 3000);
+app.set("views", `${__dirname}/views`);
 
 // Configure app and get it running
 app = config(app);
 
-var server = app.listen(app.get('port'), ()=>{
-    console.log('Server up: http://localhost:' + app.get('port'));
+var server = app.listen(app.get("port"), () => {
+  console.log("Server up: http://localhost:" + app.get("port"));
 });
